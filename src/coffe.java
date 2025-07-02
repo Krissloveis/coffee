af8d31ddd1241de8b0b915aeb7b7dd149226a4e4
krissloveis <yorkyy@mail.ru>
2025-07-02 22:12:19 +0300

    initial commit

--- src/coffe.java
+++ src/coffe.java
@@ -1,19 +1,39 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class coffe {
     public static void main(String[] args) {
         int coffeeAmount = 2330;
         int milkAmount = 3210;
         int skinnedMilkAmount = 1290;
 
-        boolean isBlocked = true;
+        boolean isBlocked = false;
 
         int cappucinoMilkRequired = 60;
         int cappucinoCoffeeRequired = 15;
 
-        if (!isBlocked && milkAmount >= cappucinoMilkRequired &&
-            coffeeAmount >= cappucinoCoffeeRequired) {
+        boolean milkIsEnought = skinnedMilkAmount >= cappucinoMilkRequired ||
+                milkAmount >= cappucinoMilkRequired;
+
+        boolean coffeIsEnought = coffeeAmount >= cappucinoCoffeeRequired;
+
+        boolean hasErrors = false;
+
+        if (isBlocked) {
+            System.out.println("Кофе-машина заблокирована");
+            hasErrors = true;
+        }
+        if (!coffeIsEnought) {
+            System.out.println("Кофе недостаточно");
+            hasErrors = true;
+
+        }
+        if (!milkIsEnought) {
+            System.out.println("Молока не достаточно");
+            hasErrors = true;
+        }
+
+        if (!hasErrors) {
             System.out.println("Готовим кофе");
-        } else {
-            System.out.println("Что-то пошло не так ;(");
         }
     }
 }
